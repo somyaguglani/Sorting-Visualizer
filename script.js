09eb22a9891b1ed49e1c6be681e48de1792c9b9a
Somya Guglani <somyaguglani0897@gmail.com>
2020-03-26 11:27:25 +0530

    reset array function done

--- script.js
+++ script.js
@@ -3,3 +3,30 @@ const generateNewArray = document.querySelector(`#generateArray`);
 const changeSize = document.querySelector(`#changeSize`);
 const buttonsArray = document.querySelectorAll(`.algoButton`);
 const sortButton = document.querySelector(`#sortButton`);
+const MAX_INTERVAL_VALUE = 1000;
+const MIN_INTERVAL_VALUE = 5;
+
+let stateArray = [];
+
+//-------------FUNCTION FOR RESETTING ARRAY----------
+
+async function resetArray() {
+  console.log(`works.....`);
+  const currentArray = [];
+  for (let i = 0; i < 100; i++) {
+    currentArray.push(
+      randomNumberFromInterval(MIN_INTERVAL_VALUE, MAX_INTERVAL_VALUE)
+    );
+  }
+  stateArray = [...currentArray];
+  console.log(stateArray);
+}
+
+function randomNumberFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+resetArray();
+
+//------------EVENT LISTENERTS--------------
+
+generateNewArray.addEventListener(`click`, resetArray);
