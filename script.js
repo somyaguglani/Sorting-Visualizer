bdac7470e940c93bb6c97d22da8c604addeb8b8c
Somya Guglani <somyaguglani0897@gmail.com>
2020-03-26 11:10:17 +0530

    selected things from dom

---
+++ script.js
@@ -0,0 +1,5 @@
+// -----------SELECTING THINGS FROM DOM-------------
+const generateNewArray = document.querySelector(`#generateArray`);
+const changeSize = document.querySelector(`#changeSize`);
+const buttonsArray = document.querySelectorAll(`.algoButton`);
+const sortButton = document.querySelector(`#sortButton`);
